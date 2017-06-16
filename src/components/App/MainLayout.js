ea68cfa2602cf595c100d1f4d2b8aefdc4503ba6
ethankhoa <ethan.huynh@gmail.com>
2017-06-16 13:47:13 +1000

    fixes nav

--- src/components/App/MainLayout.js
+++ src/components/App/MainLayout.js
@@ -3,7 +3,7 @@ import { Switch, Route } from 'react-router-dom';
 import KioskMode from '../Members/KioskMode';
 
 import { Col } from 'react-bootstrap';
-// import NavMain from '../Nav/NavMain';
+import NavMain from '../Nav/NavMain';
 import NavLeft from '../Nav/NavLeft';
 import Main from '../Main/Main';
 import EmptyLayout from './EmptyLayout';
@@ -14,20 +14,13 @@ class MainLayout extends Component {
   render() {
     return (
       <div>
-        {/* <Switch>
-          <Route exact path='/kiosk' render={() => (<KioskMode/>)}/>
-        </Switch> */}
-          {/* <NavMain /> */}
+          <NavMain />
             <Col xs={6} md={2}>
               <NavLeft />
             </Col>
             <Col xs={12} md={10}>
               <Main />
             </Col>
-
-      {/* <div>
-        <EmptyLayout />
-      </div> */}
       </div>
     );
   }
